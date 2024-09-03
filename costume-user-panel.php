b91949f4b9fc66c63b04347127a280d094fb3b5c
Rasoul <rasolmarzban@outlook.com>
2024-09-03 16:19:28 +0330

    initial

---
+++ costume-user-panel.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Plugin Name: Costume User Panle
+Plugin URI: https://github.com/rasolmarzban
+Description:  Costum User Panel
+Version: 0.0.1
+Requires at least: 1.0
+Requires PHP: 5.6.20
+Author: rasolmarzban <rasolmarzban@outlook.com>
+Author URI: https://github.com/rasolmarzban
+License: GPLv2 or later
+Text Domain: costume-user-panel
+*/
